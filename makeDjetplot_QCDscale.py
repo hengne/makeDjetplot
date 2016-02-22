1cdb6c2548955a6772f451e87e52d21265c29f7a
Hengne <Hengne.Li@CERN.CH>
2016-02-22 07:34:50 +0100

    add QCDscale scripts

--- makeDjetplot_QCDscale.py
+++ makeDjetplot_QCDscale.py
@@ -176,6 +176,134 @@ class TreePlot(Plot):
         return eff_jec
 
 
+    def vbf_eff_qcd(self, bins=None):
+        # QCD scale variations 
+        # +-------+---------------+-------------------+
+        # | Index | LHE Weight ID |  LHE Weight Name  |
+        # +-------+---------------+-------------------+
+        # |   0   |      1001     |    muR=1 muF=1    |
+        # |   1   |      1002     |    muR=1 muF=2    |
+        # |   2   |      1003     |   muR=1 muF=0.5   |
+        # |   3   |      1004     |    muR=2 muF=1    |
+        # |   4   |      1005     |    muR=2 muF=2    |
+        # |   5   |      1006     |   muR=2 muF=0.5   |
+        # |   6   |      1007     |   muR=0.5 muF=1   |
+        # |   7   |      1008     |   muR=0.5 muF=2   |
+        # |   8   |      1009     |  muR=0.5 muF=0.5  |
+        # +-------+---------------+-------------------+ 
+
+
+        t = ROOT.TChain("ZZTree/candTree")
+        for filename in self.filenames:
+            t.Add(filename)
+
+        n_pass_ct = {}
+        n_fail_ct = {}
+        n_pass_ct_sumw2 = {}
+        n_fail_ct_sumw2 = {}
+        eff_ct = {}
+        eff_ct_err = {}
+        n_pass_qcd = {}
+        n_fail_qcd = {}
+        n_pass_qcd_sumw2 = {}
+        n_fail_qcd_sumw2 = {}
+        eff_qcd = {}
+        eff_qcd_err = {}
+        for bin in bins:
+            n_pass_ct[bin] = 0.0
+            n_fail_ct[bin] = 0.0
+            n_pass_ct_sumw2[bin] = 0.0
+            n_fail_ct_sumw2[bin] = 0.0
+            eff_ct[bin] = 0.0
+            eff_ct_err[bin] = 0.0
+            n_pass_qcd[bin] = [0.0 for i in range(9)] # n pass with weights for 9 QCD scale variations.
+            n_fail_qcd[bin] = [0.0 for i in range(9)] # n fail with weights for 9 variations
+            n_pass_qcd_sumw2[bin] = [0.0 for i in range(9)] # n pass with weights for 9 QCD scale variations.
+            n_fail_qcd_sumw2[bin] = [0.0 for i in range(9)] # n fail with weights for 9 variations
+            eff_qcd[bin] = [0.0 for i in range(9)] # eff for 9 variations
+            eff_qcd_err[bin] = [0.0 for i in range(9)] # eff errors for 9 variations
+
+        print 'Processing ',self.title
+        length = t.GetEntries()
+
+        for i, entry in enumerate(t):
+            #t.GetEntry(i)
+
+            # get weights
+            wt = entry.genHEPMCweight
+
+            # qcd scale weights
+            wts = []
+            wts.append(entry.LHEweight_QCDscale_muR1_muF1)
+            wts.append(entry.LHEweight_QCDscale_muR1_muF2)
+            wts.append(entry.LHEweight_QCDscale_muR1_muF0p5)
+            wts.append(entry.LHEweight_QCDscale_muR2_muF1)
+            wts.append(entry.LHEweight_QCDscale_muR2_muF2)
+            wts.append(entry.LHEweight_QCDscale_muR2_muF0p5)
+            wts.append(entry.LHEweight_QCDscale_muR0p5_muF1)
+            wts.append(entry.LHEweight_QCDscale_muR0p5_muF2)
+            wts.append(entry.LHEweight_QCDscale_muR0p5_muF0p5)
+
+            # check pass cut or not
+            try:
+                pass_ct = entry.nCleanedJetsPt30>=2 and 1.0/(1.0+entry.phjj_VAJHU_old/entry.pvbf_VAJHU_old)>0.5
+            except ZeroDivisionError:
+                pass_ct = False
+                pass
+
+            if (i+1) % 10000 == 0 or i+1 == length:
+                print (i+1), "/", length
+
+            for bin in bins:
+                if bin.min < entry.ZZMass < bin.max:
+                    if pass_ct:
+                        n_pass_ct[bin] += wt
+                        n_pass_ct_sumw2[bin] +=wt*wt
+                        for ibb in range(9): 
+                            n_pass_qcd[bin][ibb] += wt*wts[ibb]
+                            n_pass_qcd_sumw2[bin][ibb] += wt*wts[ibb]*wt*wts[ibb]
+                    else:
+                        n_fail_ct[bin] += wt
+                        n_fail_ct_sumw2[bin] +=wt*wt
+                        for ibb in range(9):
+                            n_fail_qcd[bin][ibb] += wt*wts[ibb]
+                            n_fail_qcd_sumw2[bin][ibb] += wt*wts[ibb]*wt*wts[ibb]
+
+        for bin in bins:
+            try:
+                eff_ct[bin] = n_pass_ct[bin]/(n_pass_ct[bin]+n_fail_ct[bin])
+                eff_ct_err[bin] = sqrt((n_fail_ct[bin]**2*n_pass_ct_sumw2[bin]+n_pass_ct[bin]**2*n_fail_ct_sumw2[bin])/(n_fail_ct[bin]**4))
+            except ZeroDivisionError:
+                eff_ct[bin] = 0.0
+                eff_ct_err[bin] = 0.0
+                pass
+
+            # eff for 9 variations
+            for ibb in range(9):
+                try:
+                    eff_qcd[bin][ibb] = n_pass_qcd[bin][ibb]/(n_pass_qcd[bin][ibb]+n_fail_qcd[bin][ibb])
+                    eff_qcd_err[bin][ibb] = sqrt((n_fail_qcd[bin][ibb]**2*n_pass_qcd_sumw2[bin][ibb]+n_pass_qcd[bin][ibb]**2*n_fail_qcd_sumw2[bin][ibb])/(n_fail_qcd[bin][ibb]**4))
+                except ZeroDivisionError:
+                    eff_qcd[bin][ibb] = 0.0
+                    eff_qcd_err[bin][ibb] = 0.0
+                    pass
+
+        print '#####################################################'
+        print 'bin       eff_ct[bin]    eff_qcd[bin][0]   eff_ct_err[bin]   eff_qcd_err[bin][0]  '        
+        for i,bin in enumerate(bins):
+            print ' '+str(i)+'  '+str(eff_ct[bin])+'  '+str(eff_qcd[bin][0])+'  '+str(eff_ct_err[bin])+'  '+str(eff_qcd_err[bin][0])
+        print '#####################################################'
+
+        print '#####################################################'
+        print ' [bin][i] :     eff_qcd[bin][i]     eff_qcd_err[bin][i]  '
+        for i,bin in enumerate(bins):
+            for ib in range(9):
+                print ' ['+str(i)+']['+str(ib)+']:  '+str(eff_qcd[bin][ib])+'  '+str(eff_qcd_err[bin][ib])
+        print '#####################################################'
+
+        return eff_qcd,eff_qcd_err
+
+
 class ZXPlot(Plot):
     def __init__(self, color, style):
         Plot.__init__(self, "Z+X", color, style, "AllData")
@@ -578,10 +706,195 @@ def makeJECTable(massbins, outroot,  *plots):
     print r"\end{center}"
     print r"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 
+
+
+def makeQCDTable(massbins, outroot,  *plots):
+
+    outfile = ROOT.TFile(outroot, "RECREATE") 
+    print massbins
+    bins = [Bin(massbins[i], massbins[i+1]) for i in range(len(massbins)-1)]
+    for a in bins: print a
+
+    eff = collections.OrderedDict()
+    eff_err = collections.OrderedDict()
+    x = {}
+    y = {}
+    ex = {}
+    ey = {}
+    nbins = {}
+    g = {}
+    mg = ROOT.TMultiGraph()
+    mg.SetName('mg')
+
+    eff_up = collections.OrderedDict()
+    eff_dn = collections.OrderedDict()
+    eff_unc_up = collections.OrderedDict()
+    eff_unc_dn = collections.OrderedDict()
+    unc_ex_up = {}
+    unc_ex_dn = {}
+    unc_ey_up = {}
+    unc_ey_dn = {}
+    unc_g = {}
+    unc_mg = ROOT.TMultiGraph()
+    unc_mg.SetName('mg_unc')
+
+    legend = ROOT.TLegend(0.6, 0.4, 0.9, 0.8)
+    legend.SetName("legend")
+    legend.SetLineStyle(0)
+    legend.SetLineColor(0)
+    legend.SetFillStyle(0)
+
+    for plot in plots:
+        x[plot] = array.array("d")
+        y[plot] = array.array("d")
+        ex[plot] = array.array("d")
+        ey[plot] = array.array("d")
+        nbins[plot] = 0
+        eff[plot] = collections.OrderedDict()
+        eff_err[plot] = collections.OrderedDict()
+        eff_up[plot] = collections.OrderedDict()
+        eff_dn[plot] = collections.OrderedDict()
+        eff_unc_up[plot] = collections.OrderedDict()
+        eff_unc_dn[plot] = collections.OrderedDict()
+        unc_ex_up[plot] = array.array("d")
+        unc_ex_dn[plot] = array.array("d")
+        unc_ey_up[plot] = array.array("d")
+        unc_ey_dn[plot] = array.array("d")
+
+        eff_qcd,eff_qcd_err = plot.vbf_eff_qcd(bins)
+
+        for bin in bins:
+            eff[plot][bin] = eff_qcd[bin][0] # eff at QCD scale center
+            eff_err[plot][bin] = eff_qcd_err[bin][0] # eff stats.error at QCD scale center
+            eff_up[plot][bin] = max(eff_qcd[bin]) # biggest  eff in 9 qcd scale variations
+            eff_dn[plot][bin] = min(eff_qcd[bin]) # smallest eff in 9 qcd scale variations
+            eff_unc_up[plot][bin] = abs(eff_up[plot][bin]-eff[plot][bin])
+            eff_unc_dn[plot][bin] = abs(eff_dn[plot][bin]-eff[plot][bin])
+            if plot.title == "ttH" and bin.min >= 500: continue
+            nbins[plot] += 1
+            x[plot].append(bin.center)
+            y[plot].append(eff[plot][bin])
+            ex[plot].append(bin.error)
+            ey[plot].append(eff_err[plot][bin]) 
+            unc_ex_up[plot].append(bin.error)
+            unc_ex_dn[plot].append(bin.error)
+            unc_ey_up[plot].append(eff_unc_up[plot][bin])
+            unc_ey_dn[plot].append(eff_unc_dn[plot][bin])
+ 
+        g[plot] = ROOT.TGraphErrors(nbins[plot], x[plot], y[plot], ex[plot], ey[plot])
+        g[plot].SetName('gr_'+plot.title)
+        mg.Add(g[plot])
+        g[plot].SetLineColor(plot.color)
+        g[plot].SetMarkerColor(plot.color)
+        legend.AddEntry(g[plot], plot.title, "lp")
+        if plot.title != "Z+X" : 
+            unc_g[plot] = ROOT.TGraphAsymmErrors(nbins[plot], x[plot], y[plot], unc_ex_dn[plot], unc_ex_up[plot], unc_ey_dn[plot], unc_ey_up[plot])
+            unc_g[plot].SetName('gr_unc_'+plot.title)
+            unc_g[plot].SetMarkerColorAlpha(plot.color, 0)
+            unc_g[plot].SetLineColorAlpha(plot.color, 0)
+            unc_g[plot].SetFillColor(plot.color)
+            unc_g[plot].SetFillStyle(plot.style)
+            unc_mg.Add(unc_g[plot])
+
+    legend.AddEntry(unc_g[plots[0]],"QCD scale uncertainties", "f")
+
+    c1 = ROOT.TCanvas('c1','c1')
+    pt =ROOT.TPaveText(0.1577181,0.9562937,0.9580537,0.9947552,"brNDC")
+    pt.SetName('pt_cms')
+    pt.SetBorderSize(0)
+    pt.SetTextAlign(12)
+    pt.SetFillStyle(0)
+    pt.SetTextFont(42)
+    pt.SetTextSize(0.03)
+    text = pt.AddText(0.05,0.3,"CMS Simulation                   #sqrt{s} = 13 TeV")
+    #text = pt.AddText(0.15,0.3,"CMS Preliminary")
+    #text = pt.AddText(0.55,0.3,"#sqrt{s} = 13 TeV, L = 2.26 fb^{-1}")
+
+    mg.Draw("AP")
+    unc_mg.Draw("E2")
+    legend.Draw()
+    mg.GetXaxis().SetTitle("m_{4l} (GeV)")
+    mg.GetYaxis().SetTitle("VBF-tag Efficiency")
+    mg.GetYaxis().SetRangeUser(0,0.45)
+    pt.Draw()
+    c1.Update()
+    c1.SaveAs(plotloc+"VBFeffQCD.png")
+    c1.SaveAs(plotloc+"VBFeffQCD.eps")
+    c1.SaveAs(plotloc+"VBFeffQCD.root")
+    c1.SaveAs(plotloc+"VBFeffQCD.pdf")
+
+    # write to file
+    outfile.cd()
+    c1.Write()
+    mg.Write()
+    unc_mg.Write()
+    legend.Write()
+    pt.Write()
+    for plot in plots:
+        g[plot].Write()
+        unc_g[plot].Write()
+
+    c2 = ROOT.TCanvas('c2','c2')
+    mg.Draw("AP")
+    unc_mg.Draw("E2")
+    legend.Draw()
+    mg.GetXaxis().SetTitle("m_{4l} (GeV)")
+    mg.GetYaxis().SetTitle("VBF-tag Efficiency")
+    mg.GetYaxis().SetRangeUser(0,0.45)
+    mg.GetXaxis().SetRangeUser(100.0, 200.0)
+    pt.Draw()
+    c2.Update()
+    c2.SaveAs(plotloc+"VBFeffQCD100.png")
+    c2.SaveAs(plotloc+"VBFeffQCD100.eps")
+    c2.SaveAs(plotloc+"VBFeffQCD100.root")
+    c2.SaveAs(plotloc+"VBFeffQCD100.pdf")
+
+
+    print r"%%%%%%%%%%%%%% VBF-tag Efficiency %%%%%%%%%%%%%%%%%%%%%%%"
+    print r"\begin{center}"
+    print r"\begin{tabular}{ |%s| }" % ("|".join("c" * (len(plots)+1)))
+
+    #http://stackoverflow.com/a/9536084
+    header_format = " & ".join(["{:>15}"] * (len(plots) + 1)) + r" \\"
+    row_format = " & ".join(["{:>15}"] + [r"{:14.2f}\%"] * (len(plots))) + r"\\"
+    print r"\hline"
+    print header_format.format("", *plots)
+    for bin in bins:
+        print r"\hline"
+        print row_format.format(bin, *(eff[plot][bin]*100 for plot in plots))
+    print r"\hline"
+    print r"\end{tabular}"
+    print r"\end{center}"
+    print r"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+
+    print r"%%%%%%%%%%%%%% VBF-tag Efficiency with QCD Scale %%%%%%%%%%%%%%%"
+    print r"\begin{center}"
+    print r"\begin{tabular}{ |%s| }" % ("|".join("c" * (len(plots)+1)))
+
+    #http://stackoverflow.com/a/9536084
+    header_format = " & ".join(["{:>15}"] * (len(plots) + 1)) + r" \\"
+    row_format = " & ".join(["{:>15}"] + [r"{:14.2f}\%"] * (len(plots))) + r"\\"
+    print r"\hline"
+    print header_format.format("", *plots)
+    for bin in bins:
+        print r"\hline"
+        print row_format.format(bin, *(eff[plot][bin]*100 for plot in plots))
+        print row_format.format('$epsilon(up)$', *(eff_up[plot][bin]*100 for plot in plots))
+        print row_format.format('$epsilon(dn)$', *(eff_dn[plot][bin]*100 for plot in plots))
+        print row_format.format('$\delta\epsilon(QCD)/\epsilon$', *((eff_unc_up[plot][bin]+eff_unc_dn[plot][bin])/2.0/eff[plot][bin]*100 for plot in plots))
+    print r"\hline"
+    print r"\end{tabular}"
+    print r"\end{center}"
+    print r"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+
+
 if __name__ == "__main__":
     forplot = False
     fortable = False
-    doJEC = True
+    doJEC = False
+    doQCD = True
     if forplot:
         plots = (
                  TreePlot("VBF",  1,              "VBFH125"),
@@ -596,13 +909,13 @@ if __name__ == "__main__":
                  ZXPlot(7),
                 )
         makeDjetplots(*plots)
-    elif fortable or doJEC:
+    elif fortable or doJEC or doQCD:
         plots = (
 
-                 TreePlot("VBF",  1, 3003, 
-                    "VBFH125",
+#                 TreePlot("VBF",  1, 3003, 
+#                    "VBFH125",
 #                    "VBFH125","VBFH124", "VBFH125", "VBFH126", "VBFH130", "VBFH135", "VBFH140", "VBFH155", "VBFH160", "VBFH165", "VBFH170", "VBFH175", "VBFH200", "VBFH210", "VBFH230", "VBFH250", "VBFH270", "VBFH300", "VBFH350", "VBFH400", "VBFH450", "VBFH500", "VBFH550", "VBFH600", "VBFH700", "VBFH750", "VBFH800", "VBFH900", "VBFH1000", 
-                         ),
+#                         ),
 #                 TreePlot("ggH", 2, 3004, 
 #                    "ggH125",
 #                    "ggH115", "ggH120", "ggH124", "ggH125", "ggH126", "ggH130", "ggH135", "ggH140", "ggH145", "ggH150", "ggH155", "ggH160", "ggH165", "ggH170", "ggH175", "ggH180", "ggH190", "ggH210", "ggH230", "ggH250", "ggH270", "ggH300", "ggH350", "ggH400", "ggH450", "ggH500", "ggH550", "ggH600", "ggH700", "ggH800", "ggH900", "ggH1000",
@@ -617,14 +930,17 @@ if __name__ == "__main__":
 #                          ),
 #                 TreePlot("ttH",  4, 3012,  "ttH125"),
 #                 TreePlot("qqZZ", 6, 3013,  "ZZTo4l"),
-#                 TreePlot("ggZZ", ROOT.kViolet-1, 3016, #"ggZZ2e2mu", 
+                 TreePlot("ggZZ", ROOT.kViolet-1, 3016, 
+                     "ggZZ2e2mu", 
 #                    "ggZZ2e2mu", "ggZZ2e2tau", "ggZZ2mu2tau", "ggZZ4e", "ggZZ4mu", "ggZZ4tau", 
-#                           ),
+                           ),
 #                 ZXPlot(7, 3017),
                 )
         if fortable:
             makeDjettable([100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 300, 400, 500, 600, 700, 800, 900, 1000], *plots)
         elif doJEC:
-#            makeJECTable([100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 300, 400, 500, 600, 700, 800, 900, 1000], *plots)
-            makeJECTable([100, 150],  'outjec.root', *plots)
+            makeJECTable([100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 300, 400, 500, 600, 700, 800, 900, 1000], *plots)
+        elif doQCD:
+#            makeQCDTable([100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 300, 400, 500, 600, 700, 800, 900, 1000], *plots)
+            makeQCDTable([100, 500],  'outqcd.root', *plots)
 
